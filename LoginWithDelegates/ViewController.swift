1435344f8f548abdc9d44dcd7d7f3361b63f89e5
cotten-blackwell <cotten.blackwell@gmail.com>
2015-11-07 13:42:52 -0800

    Initial Commit

---
+++ LoginWithDelegates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginWithDelegates
+//
+//  Created by Cotten Blackwell on 11/7/15.
+//  Copyright © 2015 Cotten Blackwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
